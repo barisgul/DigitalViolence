d85482ca59d245b35cd3e59fa41ed8e22687e508
hakanturgay <turgayh@itu.edu.tr>
2021-02-08 23:49:08 +0300

    finish

--- screenshot.js
+++ screenshot.js
@@ -2,30 +2,66 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-console.log("Started to working");
-let currentUrl = null;
-
+const Url = "https://35sooetx1a.execute-api.eu-west-1.amazonaws.com/python-mail"
 function setScreenshotUrl(url) {
-  document.getElementById('target').src = url; 
+  document.getElementById('target').src = url;
   //console.log("setScreenshotUrl:"+url);
-}   
+}
 
-function setCurrentUrl(url){ 
+function setCurrentUrl(url) {
   currentUrl = url;
-  console.log("ActiveUrl:"+url);
-  console.log("CurrentUrl:"+currentUrl);
-  document.getElementById("url").innerHTML =currentUrl;
-}   
+  document.getElementById("url").innerHTML = currentUrl;
+}
 
-document.addEventListener('DOMContentLoaded', function(){
+function setCurrentUrl(url) {
+  currentUrl = url;
+  console.log("ActiveUrl:" + url);
+  console.log("CurrentUrl:" + currentUrl);
+  document.getElementById("url").innerHTML = currentUrl;
+}
+
+function prepareData() {
+  return new Promise(function (resolve, reject) {
+
+    data = {
+      img_code: document.getElementById('target').src,
+      name_surname: document.getElementsByName('name_surname')[0].value,
+      subject: document.getElementsByName('subject')[0].value,
+      message: document.getElementsByName('message')[0].value,
+      email: document.getElementsByName('email')[0].value,
+      url: document.getElementById('url').innerHTML
+
+    }
+    if (data)
+      resolve(data)
+    else
+      reject({})
+
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
   var button = document.getElementById('submit');
-  var container = document.getElementById('container');
-  var imagesArray = ['cat1.jpg','cat2.jpg','cat3.jpg','cat4.jpg','cat5.jpg'] 
-  
 
-  button.addEventListener('click',function(){
-      alert("test");
-      console.log("click me");
-      },false);
-  },false);
- 
+
+  button.addEventListener('click', function (e) {
+    e.preventDefault()
+    prepareData()
+      .then((re) => {
+
+        var request = new XMLHttpRequest();
+
+        request.open("POST", Url, true);
+        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        request.setRequestHeader("Access-Control-Allow-Origin", Url)
+        request.setRequestHeader("Access-Control-Allow-Origin", "*")
+
+        request.send(JSON.stringify(re));
+
+      })
+      .catch((er) => { ; console.log(er); })
+
+    console.log("click me");
+  }, false);
+}, false);
+
